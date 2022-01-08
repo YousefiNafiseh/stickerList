aa2d56cc104501b0dc2b7f3328cdbc62fdbc6469
YousefiNafiseh <yousefinafiseh@gmail.com>
2022-01-08 21:27:55 +0330

    add title for first page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ import * as React from 'react';
 import { Container, CardActionArea, CardMedia, CardContent, Typography, CssBaseline, Box, styled, Grid, Card } from '@mui/material';
 import Link from 'next/link';
 
-const CardComponent = styled(Card)(({ }) => ({
-  textAlign: 'center'
-}));
-
 const MainContainer = styled(Container)(({ }) => ({
-  padding: 10
+  padding: 10,
+  textAlign: 'center'
 }));
 
 export default function Home(props) {
@@ -16,11 +13,11 @@ export default function Home(props) {
     return categories.map(category => (
       <Link href={`/category/${category.name.replace(' ', '-')}`} key={category.id}>
         <Grid item xs={2} sm={4} md={4}>
-          <CardComponent sx={{ maxWidth: 345 }} >
+          <Card sx={{ maxWidth: 345 }} >
             <CardActionArea>
               <CardMedia
                 component='img'
-                height='130'
+                height='100'
                 image={category.imageUrl}
                 alt='green iguana'
               />
@@ -30,7 +27,7 @@ export default function Home(props) {
                 </Typography>
               </CardContent>
             </CardActionArea>
-          </CardComponent>
+          </Card>
         </Grid>
       </Link>
     ))
@@ -41,6 +38,7 @@ export default function Home(props) {
       <CssBaseline />
       <MainContainer>
         <Box sx={{ flexGrow: 1 }}>
+          <h4>select a category</h4>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             {renderCategories(props.categories)}
           </Grid>
